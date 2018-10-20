aaf5c8c905313baaca5a5608bbc89228468e92e6
Lorys LOPEZ <llopez@e3r1p12.42.fr>
2018-10-20 20:54:34 +0200

    try to fix list

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: llopez <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/09/24 19:11:46 by llopez            #+#    #+#             */
-/*   Updated: 2018/09/29 22:40:57 by llopez           ###   ########.fr       */
+/*   Updated: 2018/10/20 20:09:21 by llopez           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,10 @@ t_tube	**realloc_links(t_tube *tube, t_tube *add)
 
 	length = 0;
 	while (tube->links && tube->links[length])
+	{
+		printf("links n%d : %s\n", length, tube->links[length]->name);
 		length++;
+	}
 	links_tmp = malloc(sizeof(t_tube) * length + 2);
 	links_tmp[length + 1] = NULL;
 	links_tmp[length] = add;
@@ -66,7 +69,9 @@ int		main(int argc, char **argv)
 	char	**ending_room;
 	t_tube	*tube;
 	char	**split_tmp;
+	int		i;
 
+	i = 0;
 	tube = malloc(sizeof(t_tube));
 	tube->prev = NULL;
 	tube->next = NULL;
@@ -81,49 +86,48 @@ int		main(int argc, char **argv)
 			continue;
 		else if (!ft_strcmp("##start", line))
 		{
+			free(line);
 			get_next_line(0, &line);
+			ft_printf("%s\n", line);
 			starting_room = ft_strsplit(line, ' ');
 			if (!find_room(starting_room[0], tube))
 				save_room(&tube, starting_room);
 		}
 		else if (!ft_strcmp("##end", line))
 		{
+			free(line);
 			get_next_line(0, &line);
+			ft_printf("%s\n", line);
 			ending_room = ft_strsplit(line, ' ');
 			if (!find_room(ending_room[0], tube))
 				save_room(&tube, ending_room);
 		}
 		else if (!ft_strchr(line, '-') && ft_strchr(line, ' '))
 		{
-			printf("ho lord !\n");
 			split_tmp = ft_strsplit(line, ' ');
 			save_room(&tube, split_tmp);
 		}
 		else if (ft_strchr(line, '-') && !ft_strchr(line, ' '))
 		{
 			split_tmp = ft_strsplit(line, '-');
-			if (find_room(split_tmp[0], tube) && find_room(split_tmp[1], tube))
+			if (find_room(split_tmp[0], tube->prev) && find_room(split_tmp[1], tube->prev))
 			{
-				printf("adding room 1\n");
-				printf("adding room 2\n");
 				find_room(split_tmp[0], tube)->links = realloc_links(find_room(split_tmp[0], tube), find_room(split_tmp[1], tube));
 				find_room(split_tmp[1], tube)->links = realloc_links(find_room(split_tmp[1], tube), find_room(split_tmp[0], tube));
-				printf("adding room finished\n");
 			}
 		}
 		free(line);
 	}
-	printf("starting room : %s\nending room : %s\n", starting_room[0], ending_room[0]);
-	printf("\n\ntube = %s\n", tube->name);
 	while (tube->prev != NULL)
 		tube = tube->prev;
 	while (tube->next != NULL)
 	{
-		printf("room : %s\n x : %d\n y : %d\n", tube->name, tube->x, tube->y);
-		while (tube->(*links))
+		printf(" %s ", tube->name);
+		i = 0;
+		while (tube->links && tube->links[i])
 		{
-			printf("	---->%s\n", tube->links->name);
-			tube->(*links)++;
+			printf("<---->%s\n", tube->links[i]->name);
+			i++;
 		}
 		tube = tube->next;
 	}
