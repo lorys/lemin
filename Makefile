7905237eb45a0c4d92f105374de5b5ebf3a20e10
Lorys LOPEZ <llopez@e3r1p14.42.fr>
2018-11-06 23:52:48 +0100

    saves

--- Makefile
+++ Makefile
@@ -6,16 +6,32 @@
 #    By: llopez <marvin@42.fr>                      +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2018/09/24 19:15:43 by llopez            #+#    #+#              #
-#    Updated: 2018/10/23 12:15:39 by llopez           ###   ########.fr        #
+#    Updated: 2018/11/06 23:13:30 by llopez           ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
-SRCS =	main.c
+SRCS =	main.c\
+		find_path.c\
+		find_room.c\
+		make_tube.c\
+		realloc_links.c\
+		save_end.c\
+		save_room.c\
+		save_start.c\
+		show_struct.c
 
 BIN = Lem-in
 
-all:
-	gcc -o $(BIN) $(SRCS) -I lem-in.h -I libft/ libft/libft.a
+FLAG = -Wall -Wextra -Werror
 
-fsani:
-	gcc -o $(BIN) $(SRCS) -I lem-in.h -I libft/ libft/libft.a -fsanitize=address
+OBJDIR = obj/
+
+$(OBJDIR)%.o: srcs/%.c libft/libft.a libft/libft.h header/lem-in.h
+	mkdir -p $(OBJDIR)
+	gcc -o $@ $(FLAG) -c $< -I libft/ -I header/
+
+OBJ = $(SRCS:%.c=$(OBJDIR)%.o)
+
+all: $(OBJ)
+	gcc -o $(BIN) $(FLAG) $(OBJ) -I libft/ -I header/ libft/libft.a
+
---
+++ header/lem-in.h
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   lem-in.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: llopez <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/09/24 19:12:06 by llopez            #+#    #+#             */
+/*   Updated: 2018/11/06 23:13:28 by llopez           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include "libft.h"
+#include "ft_printf.h"
+#include "get_next_line.h"
+
+typedef	struct	s_tube {
+	char			*name;
+	int				x;
+	int				y;
+	int				vu;
+	struct s_tube	**links; 
+	struct s_tube	*next;
+	struct s_tube	*prev;
+}				t_tube;
+
+typedef	struct	s_infos { 
+	struct s_tube	*end;
+	struct s_tube	*start;
+	int				fourmis;
+}				t_infos;
+
+t_tube		**realloc_links(t_tube *tube, t_tube *add);
+int			find_path(t_tube *room, t_infos *infos, t_tube *from);
+t_tube		*find_room(char *name, t_tube **list);
+void		make_tube(char *line, t_tube *tube);
+void		save_end(t_tube *tube, t_infos *infos);
+void		save_room(t_tube **tube, char **split_tmp);
+void		save_start(t_tube *tube, t_infos *infos);
+void		show_struct(t_tube **tube);
