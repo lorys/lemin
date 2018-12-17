2ad7d91aa453025b92fc98f477a79d9381a4c1cd
Lorys LOPEZ <llopez@e1r1p3.42.fr>
2018-12-17 12:03:30 +0100

    opti

--- srcs/find_path.c
+++ srcs/find_path.c
@@ -6,18 +6,58 @@
 /*   By: llopez <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/06 22:12:07 by llopez            #+#    #+#             */
-/*   Updated: 2018/12/06 19:03:57 by llopez           ###   ########.fr       */
+/*   Updated: 2018/12/17 12:03:06 by llopez           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "lem_in.h"
+
+static int	already_saved(t_tube *room, t_paths *paths, t_infos *infos)
+{
+	t_paths	*tmp;
+	int		count;
+	int		countc;
+	int		found;
+
+	found = 0;
+	count = 0;
+	countc = 0;
+	tmp = paths;
+	if (room == infos->end || room == infos->start)
+		return (0);
+	while (tmp->prev)
+		tmp = tmp->prev;
+	while (tmp->room && tmp->room != infos->end)
+	{
+		countc++;
+		tmp = tmp->next;
+	}
+	while (tmp)
+	{
+		if (tmp->room == room)
+			found = 1;
+		if (tmp->room == infos->end && !found)
+		{
+			count = 0;
+			found = 0;
+		}
+		else if (tmp->room == infos->end && found && count <= countc)
+			return (1);
+		else
+			count++;
+		tmp = tmp->next;
+	}
+	return (0);
+}
 
 static int	check_every_path(t_tube *room, t_paths *paths, t_infos *infos,\
 		t_tube *from)
 {
 	t_paths		*tmp;
 	int			found;
-	
+	int			ret;
+
+	ret = 0;
 	found = 0;
 	tmp = room->links;
 	while (tmp && tmp->prev)
@@ -29,11 +69,11 @@ static int	check_every_path(t_tube *room, t_paths *paths, t_infos *infos,\
 			tmp = tmp->next;
 			continue;
 		}
-		if (find_path(tmp->room, infos, room, paths))
+		if (find_path(tmp->room, infos, room, paths) && !already_saved(room, paths, infos))
 		{
 			found++;
 			realloc_paths(paths, room);
-			if (tmp->next && room->links->next)
+			if (tmp->next && room->links->next && !already_saved(room, paths, infos))
 				realloc_paths(paths, from);
 		}
 		tmp = tmp->next;
