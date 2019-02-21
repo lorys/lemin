75d317498962685e008ad2305797905fa58fe3ca
Pierre Carles <pcarles@student.42.fr>
2019-02-21 17:36:39 +0100

    Add buffer gestion to print map

--- srcs/parser/read_file.c
+++ srcs/parser/read_file.c
@@ -6,7 +6,7 @@
 /*   By: pcarles <pcarles@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/07 00:14:16 by pcarles           #+#    #+#             */
-/*   Updated: 2019/02/20 18:08:07 by pcarles          ###   ########.fr       */
+/*   Updated: 2019/02/21 17:35:21 by pcarles          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,27 @@
 #include "libft.h"
 #include "get_next_line.h"
 #include "parser.h"
+
+#define BUFFER_SIZE 65536
+
+static void		fill_buffer(char *str)
+{
+	static char		buffer[BUFFER_SIZE + 1] = {'\0'};
+	static size_t	index = 0;
+	size_t			str_len;
+
+	str_len = 0;
+	if (str == NULL || index + (str_len = ft_strlen(str)) >= BUFFER_SIZE)
+	{
+		write(1, buffer, index);
+		index = 0;
+		if (str == NULL)
+			return ;
+	}
+	str[str_len] = '\n';
+	ft_strncpy(buffer + index, str, ++str_len);
+	index += str_len;
+}
 
 static void		read_line(int fd, t_vertice **room_listp, t_infos *infos)
 {
@@ -27,7 +48,7 @@ static void		read_line(int fd, t_vertice **room_listp, t_infos *infos)
 	while (get_next_line(fd, &line) > 0 && ++nline)
 	{
 		if (*line != '\0' && line_is_valid(room_listp, infos, line, nline))
-			ft_putendl(line);
+			fill_buffer(line);
 		else
 		{
 			get_next_line(fd, NULL);
@@ -54,6 +75,7 @@ int				read_file(t_infos *infos)
 		}
 	}
 	read_line(fd, &infos->room_list, infos);
+	fill_buffer(NULL);
 	if (fd != STDIN_FILENO)
 		close(fd);
 	return (1);
